8b8361ec5d4b3dc9d2baa5cfa6f138181381172f
Ismail <bouaozan@gmail.com>
2026-03-27 17:20:26 +0100

    feat: replace heart favicon with gold letter F

--- app/icon.tsx
+++ app/icon.tsx
@@ -17,20 +17,20 @@ export default function Icon() {
           justifyContent: 'center',
         }}
       >
-        {/* Heart shape rendered as SVG */}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="#c6a65d"
-          stroke-width="2.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+        <div
+          style={{
+            fontSize: 22,
+            fontFamily: 'serif',
+            fontStyle: 'italic',
+            fontWeight: 700,
+            color: '#c6a65d',
+            lineHeight: 1,
+            marginTop: 2,
+            marginLeft: 2,
+          }}
         >
-          <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
-        </svg>
+          F
+        </div>
       </div>
     ),
     { ...size },
