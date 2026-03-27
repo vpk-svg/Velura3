8096eb008830fcd69386a7129ee8b090450107ba
Ismail <bouaozan@gmail.com>
2026-03-27 17:38:29 +0100

    Fix root layout build error by removing app/page.tsx and adding Netlify direct route fallback

--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation';
-
-// This page ensures there is a root route to fallback on if middleware fails to catch `/` on some hosting platforms like Netlify.
-export default function RootPage() {
-  redirect('/nl');
-}
